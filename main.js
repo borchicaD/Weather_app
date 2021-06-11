cb46f5a7a2194bb52baa5f8c86c95effa39ab891
borchicaD <borka.phoenix@gmail.com>
2021-06-11 11:53:11 +0200

    All set, ajax request is working (api key needed)

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-
-$('.btn').on('click', function(){
-  var city = $('input').val();
+$('button').on('click', function(){
+  var city = $("input").val();
   $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=77734ed9e6c447983b0cfbb12134387d",
-    dataType: "json"
+    url : "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=77734ed9e6c447983b0cfbb12134387d",
+    dataType : "json"
   })
-  .done(function(res){
+  .done(function(res) {
     console.log(res);
+    var text = '<h4>Grad: '+res.name+'</h4>';
+    text+='<h5>Temperatura: '+res.main.temp+'</h5>';
+    text+='<h6>Maksimalna temperatura: '+res.main.temp_max+'</h6>';
+    text+='<h6>Minimalna temperatura: '+res.main.temp_min+'</h6>';
+    text+='<h5>Vetar: '+res.wind.speed+'</h5>'
+    $('#display').html(text);
   })
 })
